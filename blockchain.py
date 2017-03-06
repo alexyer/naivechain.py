08c5ccfc75a2f7a9fd84258184794fe31aa84f67
Olexander Yermakov <mannavard1611@gmail.com>
2017-03-06 15:54:43 +0200

    The Big Bang :boom:

---
+++ blockchain.py
@@ -0,0 +1,98 @@
+import hashlib
+from typing import Optional, List
+
+from datetime import datetime
+
+
+class Block(object):
+    __slots__ = ('index', 'previous_hash', 'timestamp', 'data', 'hash')
+
+    def __init__(self, index: int, previous_hash: str, timestamp: int, data: str, hash: Optional[str]):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+    def __eq__(self, other) -> bool:
+        return self.index == other.index and self.previous_hash == other.previous_hash \
+               and self.timestamp == other.timestamp and self.data == other.data and self.hash == other.hash
+
+
+class Blockchain(object):
+    def __init__(self, debug=False):
+        self.debug = debug
+        self._blockchain = [self.generate_genesis_block()]
+
+    @property
+    def blocks(self):
+        return self._blockchain
+
+    @property
+    def length(self):
+        return len(self._blockchain)
+
+    @property
+    def genesis_block(self):
+        return self._blockchain[0]
+
+    @property
+    def latest_block(self) -> Block:
+        return self._blockchain[-1]
+
+    @staticmethod
+    def generate_genesis_block() -> Block:
+        return Block(0, '0', 1465154705, 'my genesis block!!',
+                     '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7')
+
+    @classmethod
+    def calculate_hash_for_block(cls, block: Block) -> str:
+        return cls.calculate_hash(block.index, block.previous_hash, block.timestamp, block.data)
+
+    @staticmethod
+    def calculate_hash(index: int, previous_hash: str, timestamp: int, data: str):
+        data_str = str(index) + previous_hash + str(timestamp) + data
+        return hashlib.sha256(data_str.encode('utf-8')).hexdigest()
+
+    def is_valid_new_block(self, block: Block) -> bool:
+        return self.is_valid_block(block, self.latest_block)
+
+    def is_valid_block(self, block: Block, previous_block: Block) -> bool:
+        if previous_block.index + 1 != block.index:
+            self.log('invalid index')
+            return False
+        if previous_block.hash != block.previous_hash:
+            self.log('invalid previoushash')
+            return False
+        if self.calculate_hash_for_block(block) != block.hash:
+            self.log('invalid hash')
+            return False
+        return True
+
+    def add_block(self, block: Block):
+        if self.is_valid_new_block(block):
+            self._blockchain.append(block)
+
+    def generate_new_block(self, data: str) -> Block:
+        new_index = self.latest_block.index + 1
+        new_timestamp = datetime.utcnow().timestamp()
+        new_hash = self.calculate_hash(new_index, self.latest_block.hash, new_timestamp, data)
+
+        return Block(new_index, self.latest_block.hash, new_timestamp, data, new_hash)
+
+    def is_valid_chain(self, chain: List[Block]) -> bool:
+        if self.genesis_block != chain[0]:
+            self.log('Wrong genesis block')
+            return False
+
+        return all(self.is_valid_block(chain[i+1], chain[i]) for i in range(len(chain) - 1))
+
+    def replace_chain(self, blocks: List[Block]):
+        if self.is_valid_chain(blocks) and len(blocks) > self.length:
+            self._blockchain = blocks
+        else:
+            self.log('Received blockchain invalid')
+
+    def log(self, msg):
+        if self.debug:
+            print(msg)
---
+++ tests.py
@@ -0,0 +1,107 @@
+import unittest
+from datetime import datetime
+
+from blockchain import Block, Blockchain
+
+
+class TestBlock(unittest.TestCase):
+    def test_new_block(self):
+        block = Block(1, '12', 1465154705, 'test-block', '12345')
+
+        self.assertEqual(block.index, 1)
+        self.assertEqual(block.previous_hash, '12')
+        self.assertEqual(block.timestamp, 1465154705)
+        self.assertEqual(block.data, 'test-block')
+        self.assertEqual(block.hash, '12345')
+
+    def test_block_eq(self):
+        block = Block(1, '12', 1465154705, 'test-block', '12345')
+        other_block = Block(1, '12', 1465154705, 'test-block', '12345')
+        another_block = Block(2, '12', 1465154705, 'test-block', '12345')
+
+        self.assertEqual(block, other_block)
+        self.assertNotEqual(block, another_block)
+
+
+class TestBlockchain(unittest.TestCase):
+    def setUp(self):
+        self.blockchain = Blockchain()
+
+    def test_get_genesis_block(self):
+        self.assertEqual(Block(0, '0', 1465154705, 'my genesis block!!',
+                               '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7'),
+                         Blockchain.generate_genesis_block())
+
+    def test_calculate_hash(self):
+        block = Block(0, '0', 1465154705, 'my genesis block!!',
+                      '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7')
+        self.assertEqual('816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7',
+                         Blockchain.calculate_hash_for_block(block))
+
+    def test_is_valid_new_block(self):
+        block = Blockchain.generate_genesis_block()
+        block.index = 1
+        block.previous_hash = self.blockchain.latest_block.hash
+        block.hash = Blockchain.calculate_hash_for_block(block)
+
+        self.assertTrue(self.blockchain.is_valid_new_block(block))
+
+        block.hash = 'a'
+        self.assertFalse(self.blockchain.is_valid_new_block(block))
+
+    def test_add_block(self):
+        block = Block(self.blockchain.latest_block.index + 1, self.blockchain.latest_block.hash,
+                      datetime.utcnow().timestamp(), 'new-block', None)
+        block.hash = Blockchain.calculate_hash_for_block(block)
+
+        self.blockchain.add_block(block)
+
+        self.assertEqual(block, self.blockchain.latest_block)
+
+    def test_add_block_invalid(self):
+        block = Block(self.blockchain.latest_block.index, self.blockchain.latest_block.hash,
+                      datetime.utcnow().timestamp(), 'new-block', None)
+        block.hash = Blockchain.calculate_hash_for_block(block)
+
+        self.blockchain.add_block(block)
+
+        self.assertNotEqual(block, self.blockchain.latest_block)
+
+    def test_generate_new_block(self):
+        new_block = self.blockchain.generate_new_block('new-block')
+
+        self.assertEqual(1, new_block.index)
+        self.assertEqual(Blockchain.generate_genesis_block().hash, new_block.previous_hash)
+        self.assertEqual('new-block', new_block.data)
+        self.assertEqual(Blockchain.calculate_hash_for_block(new_block), new_block.hash)
+
+    def test_is_valid_chain__wrong_genesis_block(self):
+        other_chain = Blockchain()
+        other_chain._blockchain[0] = Block(0, '0', 1465154705, 'my genesis block!!', 'aaa')
+
+        self.assertFalse(self.blockchain.is_valid_chain(other_chain.blocks))
+
+    def test_is_valid_chain__wrong_chain(self):
+        other_chain = Blockchain()
+        new_block = other_chain.generate_new_block('new-block')
+        new_block.hash = 'aaa'
+        other_chain._blockchain.append(new_block)
+
+        self.assertFalse(self.blockchain.is_valid_chain(other_chain.blocks))
+
+    def test_is_valid_chain__valid_chain(self):
+        other_chain = Blockchain()
+        other_chain.add_block(other_chain.generate_new_block('new-block'))
+        other_chain.add_block(other_chain.generate_new_block('other-new-block'))
+
+        self.assertTrue(self.blockchain.is_valid_chain(other_chain.blocks))
+
+    def replace_chain(self):
+        other_chain = Blockchain()
+        other_chain.add_block(other_chain.generate_new_block('new-block'))
+        new_latest_block = other_chain.add_block(other_chain.generate_new_block('other-new-block'))
+
+        self.blockchain.replace_chain(other_chain.blocks)
+
+        self.assertEqual(new_latest_block, self.blockchain.latest_block)
+        self.assertEqual(3, self.blockchain.length)
